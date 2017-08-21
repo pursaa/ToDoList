8bd2c4bd19d3372b1ab0d7ad608f245fbaf2c825
Debbie Michel and Kelsey Langlois <debbie@>
2017-08-21 13:53:33 -0700

    Change list item to strikethrough when item is checked completed.

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,13 @@ $(document).ready(function() {
     var task = $("input#task").val();
     $("ul#ToDoList").append("<li><input type='checkbox' name='task-complete' value='" + task + "'> " + task + "</li>");
     $("input#task").val("");
+
+    $("input:checkbox[name=task-complete]").last().change(function() {
+      if ($(this).parent().hasClass("completed")) {
+        $(this).parent().removeClass("completed");
+      } else {
+        $(this).parent().addClass("completed");  
+      }
+    });
   });
 });
